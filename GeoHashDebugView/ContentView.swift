2bd38769a06da542cf09e2b3eed9de3928095ce0
fummicc1 <fumiya.tennis1@gmail.com>
2024-11-17 00:44:07 +0900

    Show Polylines on Map

--- GeoHashDebugView/ContentView.swift
+++ GeoHashDebugView/ContentView.swift
@@ -6,16 +6,68 @@
 //
 
 import SwiftUI
+import MapKit
+import CoreLocation
+import GeoHashFramework
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    @State var bitsLength: Int = 5
+    var bounds: [[CLLocationCoordinate2D]] {
+        GeoHash.getBounds(with: .exact(digits: bitsLength)).map {
+            $0.map {
+                CLLocationCoordinate2D(
+                    latitude: $0.latitude,
+                    longitude: $0.longitude
+                )
+            }
         }
-        .padding()
+    }
+    
+    var body: some View {
+        Map{
+            ForEach(bounds) { bound in
+                Group {
+                    MapPolyline(
+                        coordinates: [
+                            bound[0],
+                            bound[1]
+                        ]
+                    )
+                    MapPolyline(
+                        coordinates: [
+                            bound[1],
+                            bound[2]
+                        ]
+                    )
+                    MapPolyline(
+                        coordinates: [
+                            bound[2],
+                            bound[3]
+                        ]
+                    )
+                    MapPolyline(
+                        coordinates: [
+                            bound[3],
+                            bound[0]
+                        ]
+                    )
+                }
+                .stroke(.green, style: .init(lineWidth: 4))
+            }
+        }
+    }
+}
+
+extension Array: @retroactive Identifiable where Element: Identifiable<String> {
+    public var id: String {
+        map { $0.id }.joined(separator: "|")
+    }
+}
+
+extension CLLocationCoordinate2D: @retroactive Identifiable {
+    public var id: String {
+        "\(latitude),\(longitude)"
     }
 }
 
