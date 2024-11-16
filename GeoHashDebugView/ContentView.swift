660ed1e8eb3d5fd3adb120da079ee4f567280e1f
fummicc1 <fumiya.tennis1@gmail.com>
2024-11-17 02:23:30 +0900

    Add

--- GeoHashDebugView/ContentView.swift
+++ GeoHashDebugView/ContentView.swift
@@ -11,51 +11,57 @@ import CoreLocation
 import GeoHashFramework
 
 struct ContentView: View {
+    @State var bitsLength: Int = 10
+    @State private var region = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 0, longitude: 0),
+            span: MKCoordinateSpan(latitudeDelta: 360, longitudeDelta: 360)
+        )
+    )
     
-    @State var bitsLength: Int = 5
     var bounds: [[CLLocationCoordinate2D]] {
         GeoHash.getBounds(with: .exact(digits: bitsLength)).map {
-            $0.map {
+            $0.map { coord in
                 CLLocationCoordinate2D(
-                    latitude: $0.latitude,
-                    longitude: $0.longitude
+                    latitude: coord.latitude,
+                    longitude: coord.longitude
                 )
             }
         }
     }
     
     var body: some View {
-        Map{
+        Map(position: $region) {
             ForEach(bounds) { bound in
-                Group {
-                    MapPolyline(
-                        coordinates: [
-                            bound[0],
-                            bound[1]
-                        ]
-                    )
-                    MapPolyline(
-                        coordinates: [
-                            bound[1],
-                            bound[2]
-                        ]
-                    )
-                    MapPolyline(
-                        coordinates: [
-                            bound[2],
-                            bound[3]
-                        ]
-                    )
-                    MapPolyline(
-                        coordinates: [
-                            bound[3],
-                            bound[0]
-                        ]
+                MapPolyline(coordinates: bound)
+                    .stroke(Color.blue, lineWidth: 1)
+                Annotation(
+                    coordinate: getCenter(in: bound),
+                    content: {
+                        Text(
+                            GeoHash(
+                                latitude: getCenter(in: bound).latitude,
+                                longitude: getCenter(in: bound).longitude
+                            ).geoHash
+                        )
+                    }
+                ) {
+                    Text(
+                        GeoHash(
+                            latitude: getCenter(in: bound).latitude,
+                            longitude: getCenter(in: bound).longitude
+                        ).binary
                     )
                 }
-                .stroke(.green, style: .init(lineWidth: 4))
             }
         }
+    }
+    
+    private func getCenter(in bound: [CLLocationCoordinate2D]) -> CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: (bound[1].latitude + bound[2].latitude) / 2,
+            longitude: (bound[1].longitude + bound[0].longitude) / 2
+        )
     }
 }
 
---
+++ README.md
@@ -0,0 +1,7 @@
+# GeoHash Debug View
+
+This is a debug view for [GeoHashSwift](https://github.com/fummicc1/GeoHashSwift).
+
+## Demo
+
+![Demo](./demo.gif)
